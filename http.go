764591b70e1482e4062877e04551b9fd6e723801
Gregory Vincic <g@7de.se>
2018-04-06 12:20:30 +0200

    Add StatusWriter
    Writes the status code that ResponseWriter sends using the WriteHeader

--- http.go
+++ http.go
@@ -2,29 +2,33 @@
 package ex
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 )
 
 type BodyWriter struct {
 	header http.Header
 }
 
-func NewBodyWriter() *BodyWriter {
-	return &BodyWriter{
-		header: http.Header{},
-	}
-}
-
-// WriteHeader does nothing
-func (w *BodyWriter) WriteHeader(v int)  {}
+func NewBodyWriter() *BodyWriter          { return &BodyWriter{header: http.Header{}} }
+func (w *BodyWriter) WriteHeader(v int)   {}
+func (w *BodyWriter) Header() http.Header { return w.header }
 
 // Write the given byte slice to stdout
-func (w *BodyWriter) Write(b []byte) (int, error)  {
+func (w *BodyWriter) Write(b []byte) (int, error) {
 	fmt.Printf("%s", string(b))
 	return len(b), nil
 }
 
-func (w *BodyWriter) Header() http.Header  {
-	return w.header
+type StatusWriter struct {
+	header http.Header
 }
+
+func NewStatusWriter() *StatusWriter { return &StatusWriter{header: http.Header{}} }
+
+// Write the given value to stdout
+func (w *StatusWriter) WriteHeader(v int) {
+	fmt.Printf("%v", v)
+}
+func (w *StatusWriter) Header() http.Header         { return w.header }
+func (w *StatusWriter) Write(b []byte) (int, error) { return len(b), nil }
--- http_test.go
+++ http_test.go
@@ -4,22 +4,38 @@ import (
 	"testing"
 )
 
+var bodyWriter = NewBodyWriter()
+var statusWriter = NewStatusWriter()
+
 func ExampleBodyWriter_Write() {
-	w := NewBodyWriter()
-	w.Write([]byte("Hello, world!"))
+	bodyWriter.Write([]byte("Hello, world!"))
 	// output:
 	// Hello, world!
 }
 
 func ExampleBodyWriter_WriteHeader() {
-	w := NewBodyWriter()
-	w.WriteHeader(200)
+	bodyWriter.WriteHeader(200)
 	// output:
 }
 
 func TestBodyWriter_Header(t *testing.T) {
-	w := NewBodyWriter()
-	if w.Header() == nil {
+	if bodyWriter.Header() == nil {
+		t.Fail()
+	}
+}
+
+func ExampleStatusWriter_WriteHeader() {
+	statusWriter.WriteHeader(200)
+	// output:
+	// 200
+}
+func ExampleStatusWriter_Write() {
+	statusWriter.Write([]byte("Hello, world!"))
+	// output:
+}
+
+func TestStatusWriter_Header(t *testing.T) {
+	if statusWriter.Header() == nil {
 		t.Fail()
 	}
 }
