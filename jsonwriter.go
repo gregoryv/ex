c4ed7625164d56d1fa09246ffeb4dd1062ea82fd
Gregory Vincic <gregoryv@axis.com>
2018-12-12 14:26:27 +0100

    Refactor JsonWriter for optional destination
    So we can reuse the nice printing of json in other scenarios

--- jsonwriter.go
+++ jsonwriter.go
@@ -2,43 +2,57 @@ package ex
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 	"strings"
 )
 
 // Very basic Json tidier
 type JsonWriter struct {
 	header http.Header
+	indent int
+	inside bool
 }
 
-func NewJsonWriter() *JsonWriter          { return &JsonWriter{header: http.Header{}} }
+func NewJsonWriter() *JsonWriter {
+	return &JsonWriter{
+		header: http.Header{},
+	}
+}
 func (w *JsonWriter) WriteHeader(v int)   {}
 func (w *JsonWriter) Header() http.Header { return w.header }
 
 // Write the given byte slice to stdout
 func (w *JsonWriter) Write(b []byte) (int, error) {
-	indent := 0
-	inside := false
+	return jwrite(w, os.Stdout, b)
+}
+
+func jwrite(jw *JsonWriter, out io.Writer, b []byte) (int, error) {
 	for _, b := range b {
 		switch b {
 		case '"':
-			inside = !inside
-			fmt.Print(string(b))
+			jw.inside = !jw.inside
+			fmt.Fprint(out, string(b))
 		case ',':
-			if inside {
-				fmt.Print(string(b))
+			if jw.inside {
+				fmt.Fprint(out, string(b))
 				continue
 			}
-			fmt.Print(",\n", strings.Repeat(" ", indent))
+			fmt.Fprintf(out, ",\n%s", jw.tab())
 		case '{':
-			indent += 4
-			fmt.Print("{\n", strings.Repeat(" ", indent))
+			jw.indent += 4
+			fmt.Fprintf(out, "{\n%s", jw.tab())
 		case '}':
-			indent -= 4
-			fmt.Print("\n", strings.Repeat(" ", indent), "}")
+			jw.indent -= 4
+			fmt.Fprintf(out, "\n%s}", jw.tab())
 		default:
-			fmt.Print(string(b))
+			fmt.Fprint(out, string(b))
 		}
 	}
 	return len(b), nil
+}
+
+func (jw *JsonWriter) tab() string {
+	return strings.Repeat(" ", jw.indent)
 }
