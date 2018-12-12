795f6a4e25084eb1c7419fb16ab39a54bbf974df
Gregory Vincic <gregoryv@axis.com>
2018-12-12 10:54:50 +0100

    Add Tfprint
    Whenever there is a need to modify the actual values before printing
    a tidy func can be supplied. This is needed when eg. dynamic values
    must be replaced with fixed ones.

--- header.go
+++ header.go
@@ -8,20 +8,25 @@ import (
 	"sort"
 )
 
-func Fprint(w io.Writer, any ...interface{}) {
+// Print writes any element to stdout.
+func Print(any ...interface{}) {
+	Tfprint(asIs, os.Stdout, any...)
+}
+
+func Tprint(tidy TidyFunc, any ...interface{}) {
+	Tfprint(tidy, os.Stdout, any...)
+}
+
+func Tfprint(tidy TidyFunc, w io.Writer, any ...interface{}) {
 	for _, e := range any {
 		switch e := e.(type) {
 		case http.Header:
-			fprint(os.Stdout, e)
+			printHeaders(tidy, os.Stdout, e)
 		}
 	}
 }
 
-func Print(any ...interface{}) {
-	Fprint(os.Stdout, any...)
-}
-
-func fprint(w io.Writer, header http.Header) {
+func printHeaders(tidy TidyFunc, w io.Writer, header http.Header) {
 	keys := make([]string, 0)
 	for k := range header {
 		keys = append(keys, k)
@@ -30,7 +35,11 @@ func fprint(w io.Writer, header http.Header) {
 	for _, k := range keys {
 		v := header[k]
 		for _, v := range v {
-			fmt.Fprintf(w, "%v: %v\n", k, v)
+			fmt.Fprintf(w, "%v: %v\n", k, tidy(v))
 		}
 	}
 }
+
+type TidyFunc func(string) string
+
+func asIs(v string) string { return v }
