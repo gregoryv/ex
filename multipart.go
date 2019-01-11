c25c62173819430e86e6672af237f69c500ea6b9
Gregory Vincic <gregoryv@axis.com>
2019-01-11 12:09:49 +0100

    Make DumpParts json aware

--- multipart.go
+++ multipart.go
@@ -1,15 +1,32 @@
 package ex
 
 import (
+	"fmt"
 	"io"
 	"mime/multipart"
 	"os"
+	"strings"
 )
 
 // DumpParts prints each part of a multipart message
 func DumpParts(msg io.Reader, boundary string) {
 	mr := multipart.NewReader(msg, boundary)
+	jsonWriter := NewJsonWriter()
 	for p, _ := mr.NextPart(); p != nil; p, _ = mr.NextPart() {
-		io.Copy(os.Stdout, p)
+		if isJson(p.Header["Content-Type"]) {
+			io.Copy(jsonWriter, p)
+			fmt.Fprint(jsonWriter, "\n")
+		} else {
+			io.Copy(os.Stdout, p)
+		}
 	}
+}
+
+func isJson(contentType []string) bool {
+	for _, t := range contentType {
+		if strings.Contains(t, "application/json") {
+			return true
+		}
+	}
+	return false
 }
--- multipart_test.go
+++ multipart_test.go
@@ -6,6 +6,28 @@ import (
 	"net/textproto"
 )
 
+func ExampleDumpParts_with_nice_json() {
+	buf := bytes.NewBufferString("")
+	body := multipart.NewWriter(buf)
+	body.SetBoundary("--X")
+
+	header := textproto.MIMEHeader{}
+	header["Content-Type"] = []string{"application/json"}
+	w, _ := body.CreatePart(header)
+	w.Write([]byte(`{"text":"hello","more":"world"}`))
+
+	w, _ = body.CreatePart(textproto.MIMEHeader{})
+	w.Write([]byte("next part"))
+
+	DumpParts(buf, "--X")
+	// output:
+	// {
+	//     "text":"hello",
+	//     "more":"world"
+	// }
+	// next part
+}
+
 func ExampleDumpParts() {
 	buf := bytes.NewBufferString("")
 	body := multipart.NewWriter(buf)
