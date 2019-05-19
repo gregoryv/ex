58d5697cb05198becfb27a6f870994638ba51f21
Gregory Vincic <g@7de.se>
2019-05-19 10:36:32 +0200

    Cover WriteHeader

--- headerwriter_test.go
+++ headerwriter_test.go
@@ -34,6 +34,7 @@ func TestHeaderWriter_Header(t *testing.T) {
 
 func ExampleHeadersOf() {
 	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "Hello, %s!", "world")
 	}
 	handler(HeadersOf(http.NewRequest("GET", "/", nil)))
